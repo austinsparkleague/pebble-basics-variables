2dff02ba082d651d35eb2544f75a110eb22dcda3
Scott Brackett <scottybrackett@gmail.com>
2015-07-14 13:16:41 -0500

    more comments updates
    Cleans up Comments formatting, and adds additional descriptive text regarding pointers.

--- src/main.c
+++ src/main.c
@@ -9,22 +9,36 @@ TextLayer *text_layer;
 
 static void main_window_load(Window *window){
   //Here is where we are going to work for this project---------------------------------------
+  
   //all programs store information in memory.  In order to do so, we first need to tell C,
   //what kind of information it should expect us to be storing in that variable.
   //The main reason for this is that different types of data will require different amounts of 
   //room in memory, and we want to be very specific so we don't set aside more than we need.
-  //In most programming languages this is how you would declare a variable
+  //In most programming languages this is how you would declare a variable:
   int some_integer = 4;
   
-  //C is a very powerful language because it allows us to have a very nuanced control of our memory.
-  //When storing Data, we also make heavy use of something called a "pointer"
-  //This is a little tricky, but know for now that it helps us to set aside a chunk of memory, like so...
+  //this would instructs the computer to set aside enough space in memory to store an integer, and then 
+  //we tell 
+  
+  //C is a powerful language because it allows us to have a very nuanced control of our memory.
+  //This sometimes makes it a difficult langauge to learn, because you have to be very explicit
+  //about how you want things done at every step of the process.
+  //
+  //When storing Data, we also make heavy use of something called a "pointer", which is just a specific type of variable
+  //The concept of the pointer is actually considered an "advanced" concept, but pebble requires us
+  //to use pointers.  So we are going to talk about it sooner than I would prefer.
+  //It's a little tricky, but know for now that it helps us to set aside, and keep track of a chunk of memory, like so...
   char *i_am_a_word;
   
-  //notice that we put an * in front of the name of this variable.  That is how we specify that something is a pointer.
-  //so by entering "*i_am_a_word"" we are saying something like, "hey machine, I am going to need you to set aside enough memory for 
+  //notice that we put an * in front of the name of this variable.  That is how we specify that we want to create a pointer.
+  //so by entering "*i_am_a_word" we are saying something like, "hey machine, I am going to need you to set aside enough memory for 
   //some text content, and from here on out, when I enter "i_am_a_word" I want you to plug in the value of whatever is in that chunk of memory that
-  //you set aside." 
+  //you set aside.  And if I enter "*i_am_a_word" I want you to plug in the actual address in memory where that data is stored.
+  //
+  //so technically, by the code above created two variables.  One that will hold some text characters, and another that contains the address in memory where
+  //that text will be stored.
+  
+  //If that is hard to understand, don't worry, it will make sense later.
   
   //so now that we created a pointer to that bit of memory, we are going to assign a value to the memory that is being pointed to.
   i_am_a_word = "hello!";
