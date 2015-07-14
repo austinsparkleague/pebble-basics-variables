4f15e765924af8757f26d0c51975626dcd109bf9
Scott Brackett <scottybrackett@gmail.com>
2015-07-14 13:57:29 -0500

    more comments updates

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <pebble.h>
 /*this is an example of how variables work in C.  For now, ignore all of the set up code for the app, and we are just going to focus on a small bit of
-code that focuses on how you can store information in variables.*/
+code that will show you how to store information in variables.*/
   
   
   
@@ -10,42 +10,50 @@ TextLayer *text_layer;
 static void main_window_load(Window *window){
   //Here is where we are going to work for this project---------------------------------------
   
-  //all programs store information in memory.  In order to do so, we first need to tell C,
+  //all programs store information in memory.  In order to do so, we first need to tell the computer
   //what kind of information it should expect us to be storing in that variable.
   //The main reason for this is that different types of data will require different amounts of 
   //room in memory, and we want to be very specific so we don't set aside more than we need.
   //In most programming languages this is how you would declare a variable:
-  int some_integer = 4;
+  int some_integer;
   
-  //this instructs the computer to set aside enough space in memory to store an integer, and then 
-  //we tell it to store the integer 4 in that memory. 
+  //this instructs the computer to set aside enough space in memory to store an integer, 
   
-  //C is a powerful language because it allows us to have a very nuanced control of our memory.
-  //This can makes it an intimidating langauge to learn, because you have to be very explicit
+  some_integer = 4;
+  //Next, we told it to store the integer "4" in that chunk of memory. 
+  
+  //C is a powerful language because it allows us to have a very nuanced control of our pebble's memory.
+  //This can make it an intimidating langauge to learn, because you have to be very explicit
   //about how you want things done at every step of the process.  But fear not!
   //
-  //there are other types of variables that we can create too.  Not just integers.
- //  Here are the basic types that we can use in C:
+  //there are other types of variables that we can create too, not just integers.
+  // Here are the basic types that we can use in C:
+
 ////////////////////////////////////////////////////////////////////////////
 //   char 	This is what we will use to store words, a.k.a. "strings"     //
 //   int 	The most natural size of integer for the machine.               //
 //   float 	A single-precision floating point value.                      // 
 //   double 	A double-precision floating point value.                    //
 //   void 	Represents the absence of type.                               //
-////////////////////////////////////////////////////////////////////////////  
+////////////////////////////////////////////////////////////////////////////
   
+  //There are also some more complex types, but we'll get to those later.
+    
   //When storing Data, we also make heavy use of something called a "pointer", which is just a specific type of variable
   //The concept of the pointer is actually considered an "advanced" concept, but pebble requires us
   //to use pointers.  So we are going to talk about it sooner than I would prefer.
-  //It's a little tricky, but know for now that it helps us to set aside, and keep track of a chunk of memory, like so...
+  //It's a little tricky, but know for now that it helps us to set aside and keep track of a chunk of memory, like so...
   char *i_am_a_word;
   
-  //notice that we put an * in front of the name of this variable.  That is how we specify that we want to create a pointer.
+  // we just created a variable that will hold up to 80 characters in memory.
+  
+  //Also, by adding the * in front of the variable name, we have actually created or "declared" two variables in one step.
+  //The * that we put in front of the name of this variable, is how we specify that we want to create a pointer.
   //so by entering "*i_am_a_word" we are saying something like, "hey machine, I am going to need you to set aside enough memory for 
-  //some text content, and from here on out, when I enter "i_am_a_word" I want you to plug in the value of whatever is in that chunk of memory that
+  //some text content, and from here on out, when I enter "i_am_a_word" I want you to plug in the value of whatever is stored in the chunk of memory that
   //you set aside.  And if I enter "*i_am_a_word" I want you to plug in the actual address in memory where that data is stored.
   //
-  //so technically, by the code above created two variables.  One that will hold some text characters, and another that contains the address in memory where
+  //so technically, we have created two variables:  One that will hold some text characters, and another that contains the address in memory where
   //that text will be stored.
   
   //If that is hard to understand, don't worry, it will make sense later.
@@ -60,11 +68,15 @@ static void main_window_load(Window *window){
   
   Layer *window_layer = window_get_root_layer(window);
   text_layer = text_layer_create(GRect(0, 0, 144, 60));
+  
+  
   text_layer_set_text(text_layer, i_am_a_word );
   text_layer_set_text_color(text_layer, GColorBlack);
   
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
-
+  
+  
+  some_integer++;
 }
 
 static void main_window_unload(){
